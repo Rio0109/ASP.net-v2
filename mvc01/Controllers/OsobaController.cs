88b45236d4aa2823fd87fac618f7ba84173a642a
ivanr <ivanr@DESKTOP-09U4L3N>
2024-09-10 16:36:05 +0200

    Add project files.

---
+++ mvc01/Controllers/OsobaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using mvc01.Models;
+
+namespace mvc01.Controllers
+{
+    public class OsobaController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var osobe = new List<Osoba>
+            {
+                new Osoba {Id = 1, Ime="Ante", Prezime="Ristovski",Godine = 30},
+                new Osoba {Id = 2, Ime="Dolores",Prezime="Zaboravio sam",Godine=28}
+            };
+            return View(osobe);
+        }
+    }
+}
---
+++ mvc01/Models/Osoba.cs
@@ -0,0 +1,12 @@
+﻿namespace mvc01.Models
+{
+    public class Osoba
+    {
+
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+        public int Godine { get; set; }
+    }
+}
